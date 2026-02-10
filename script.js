a7bb5948a62272e2f1b6fdb02f8eb03515ea826f
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-10 13:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function minDate(dates) {
   //write you code here
+	let min = dates[0];
+
+	for (let i = 1; i < dates.length; i++) {
+		if(dates[i] < minDate){
+			min = dates[i];
+		}
+	}
+	return min;
 }
 
 // Do not change the code
@@ -37,4 +45,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
